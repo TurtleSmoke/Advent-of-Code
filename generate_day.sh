2fa1b1793763c03f9bf9e75901b0d82e1f18fdf7
TurtleSmoke <46165291+TuWG@users.noreply.github.com>
2022-12-03 12:19:31 +0100

    feat: add day generation script

--- generate_day.sh
+++ generate_day.sh
@@ -1,13 +1,13 @@
 #/bin/sh
 
-year=2022
-day="$1"
-dir="$year/day_$day"
+AOC_YEAR="${AOC_YEAR:-2022}"
+AOC_DAY="$1"
+dir="$AOC_YEAR/day_$AOC_DAY"
 
 [ -d "$dir" ] && echo 'Folder already exists' && exit 1
 
 mkdir "$dir"
 touch "$dir/test_input"
-touch "$dir/input"
+curl https://adventofcode.com/$AOC_YEAR/day/$AOC_DAY/input --cookie "session=$AOC_SESSION" -o "$dir/input"
 echo '#!/bin/python3' > "$dir/main.py"
 chmod u+x "$dir/main.py"
