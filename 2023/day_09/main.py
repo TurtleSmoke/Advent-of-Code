3fe186dcc731c21b0b5f241ba1c49be1202e1952
TurtleSmoke <46165291+TuWG@users.noreply.github.com>
2023-12-09 16:16:20 +0100

    feat(2023): day09

---
+++ 2023/day_09/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+input_file = "test_input"
+input_nums = [list(map(int, line.split())) for line in open(input_file).readlines()]
+
+
+def extrapolate(nums):
+    last_diff = [nums[-1]]
+    while any(diff != 0 for diff in nums):
+        nums = [nums[i] - nums[i - 1] for i in range(1, len(nums))]
+        last_diff.append(nums[-1])
+    return sum(last_diff)
+
+
+print(sum(extrapolate(nums) for nums in input_nums))
+print(sum(extrapolate(nums[::-1]) for nums in input_nums))
