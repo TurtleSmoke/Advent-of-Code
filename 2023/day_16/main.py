caf4a8200d3d5411e5ed43e89171ca0a95ddefba
TurtleSmoke <46165291+TuWG@users.noreply.github.com>
2023-12-17 15:12:35 +0100

    refactor(2023): day16

--- 2023/day_16/main.py
+++ 2023/day_16/main.py
@@ -1,54 +1,36 @@
 #!/usr/bin/env python
-import itertools
-from collections import defaultdict
 
-input_file = "input"
-
-input_data = open(input_file).readlines()
-input_size = len(input_data)
-input_mirrors = defaultdict(
-    lambda: ".",
-    {(y + x * 1j): m for y, line in enumerate(input_data) for x, m in enumerate(line.strip()) if m in r"/|\|-"},
-)
+input_file = "test_input"
+input_mirrors = {(y + x * 1j): m for y, line in enumerate(open(input_file)) for x, m in enumerate(line.strip())}
+map_dir = {"|": lambda _: 1, "-": lambda _: -1j, "\\": lambda x: 1j / x, "/": lambda x: -1j / x, ".": lambda x: x}
 
 
-def energized_grid(mirrors, start_pos, start_dir, size):
+def energized_grid(mirrors, start_pos, start_dir):
     rays = [(start_pos, start_dir)]
-    cache = defaultdict(set)
+    cache = set()
 
     while rays:
-        p, d = rays.pop()
+        cur_pos, cur_dir = rays.pop()
 
-        if (not (0 <= p.real < size and 0 <= p.imag < size)) or d in cache[p]:
-            continue
+        while not (cur_pos, cur_dir) in cache:
+            cache.add((cur_pos, cur_dir))
+            cur_pos += cur_dir
+            if cur_pos not in mirrors:
+                break
 
-        cache[p].add(d)
+            cur_dir = map_dir[mirrors[cur_pos]](cur_dir)
+            if mirrors[cur_pos] in "|-":
+                rays.append((cur_pos, -cur_dir))
 
-        if mirrors[p] == "-" and d.real != 0:
-            rays.append((p - 1j, -1j))
-            rays.append((p + 1j, 1j))
-        elif mirrors[p] == "|" and d.imag != 0:
-            rays.append((p + 1, 1))
-            rays.append((p - 1, -1))
-        else:
-            new_dir = {"/": lambda x: -complex(x.imag, x.real), "\\": lambda x: complex(x.imag, x.real)}.get(
-                mirrors[p], lambda x: x
-            )(d)
-
-            rays.append((p + new_dir, new_dir))
-
-    return len(cache)
+    return len(set(pos for pos, _ in cache)) - 1
 
 
-print(energized_grid(input_mirrors, 0 + 0j, 0 + 1j, input_size))
+print(energized_grid(input_mirrors, -1j, 1j))
 print(
     max(
-        energized_grid(input_mirrors, complex(*pos), d, input_size)
-        for pos, d in itertools.chain(
-            *(
-                (((0, k), 1), ((input_size - 1, k), -1), ((k, 0), 1j), ((k, input_size - 1), -1j))
-                for k in range(input_size)
-            )
-        )
+        energized_grid(input_mirrors, pos - d, d)
+        for d in (1, 1j, -1, -1j)
+        for pos in input_mirrors
+        if pos - d not in input_mirrors
     )
 )
