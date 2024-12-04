f173ea2b8e16d553d5289a8a5cf0bf5ae0e66dda
William Grolleau <william.grolleau@cea.fr>
2024-12-04 14:56:37 +0100

    feat: day 04

---
+++ 2024/day_04/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from collections import defaultdict
+
+input_file = "input"
+
+data = defaultdict(
+    str,
+    {
+        x + 1j * y: c
+        for y, r in enumerate(open(input_file))
+        for x, c in enumerate(r.strip())
+    },
+)
+
+res1 = 0
+res2 = 0
+
+for c in data.copy():
+    for d in [1, 1j, 1 + 1j, 1 - 1j, -1, -1j, -1 + 1j, -1 - 1j]:
+        res1 += data[c] + data[c + d] + data[c + d * 2] + data[c + d * 3] == "XMAS"
+        if d.imag and d.real:
+            res2 += (
+                data[c + d] + data[c] + data[c - d] == "MAS"
+                and data[c + d * 1j] + data[c - d * 1j] == "MS"
+            )
+
+print(res1)
+print(res2)
