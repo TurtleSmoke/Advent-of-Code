03b3da5a6ce9cbccf4d9f55b4acda0ef0238c7b8
TurtleSmoke <46165291+TuWG@users.noreply.github.com>
2024-12-05 21:54:02 +0100

    feat(2024): day 05

---
+++ 2024/day_05/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from collections import defaultdict
+
+input_file = "test_input"
+
+rules, updates = open(input_file).read().strip().split("\n\n")
+rules = set(tuple(map(int, line.split("|"))) for line in rules.split("\n"))
+updates = [tuple(map(int, line.split(","))) for line in updates.split("\n")]
+
+incorrect_update = lambda update: any((y, x) in rules for i, x in enumerate(update) for y in update[i + 1 :])
+
+res1 = sum(update[len(update) // 2] for update in updates if not incorrect_update(update))
+
+graph = defaultdict(set)
+for src, dst in rules:
+    graph[src].add(dst)
+
+res2 = 0
+for update in updates:
+    if incorrect_update(update):
+        rule = set(update)
+        valid_update = {page: graph[page] & rule for page in update}
+        valid_update = sorted(valid_update, key=lambda j: len(valid_update[j]), reverse=True)
+        res2 += int(valid_update[len(valid_update) // 2])
+
+print(res1)
+print(res2)
