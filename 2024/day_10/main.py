7c60d5e609d9793e54d847ad3d559eb6bfbc6ab5
TurtleSmoke <46165291+TuWG@users.noreply.github.com>
2024-12-11 20:35:13 +0100

    feat(2024): day 10 but smol

--- 2024/day_10/main.py
+++ 2024/day_10/main.py
@@ -1,26 +1,12 @@
 #!/usr/bin/env python
 
-from collections import deque
-
 input_file = "input"
 
 data = {y + x * 1j: int(v) for x, row in enumerate(open(input_file)) for y, v in enumerate(row.strip())}
 
-starts = [pos for pos in data if data[pos] == 0]
+starts = [[pos] for pos in data if data[pos] == 0]
+for hike in range(1, 10):
+    starts = [[p + d for p in poses for d in [1, -1, 1j, -1j] if data.get(p + d) == hike] for poses in starts]
 
-
-def dfs(start):
-    visited = [start]
-    queue = deque([start])
-    while queue:
-        current = queue.popleft()
-        for neighbor in [current + 1, current - 1, current + 1j, current - 1j]:
-            if neighbor in data and data[current] == data[neighbor] - 1:
-                visited.append(neighbor)
-                queue.append(neighbor)
-
-    return visited
-
-
-print(sum(len(set(x for x in dfs(start) if data[x] == 9)) for start in starts))
-print(sum(sum(1 for x in dfs(start) if data[x] == 9) for start in starts))
+print(sum(map(len, map(set, starts))))
+print(len(list(x for y in starts for x in y)))
