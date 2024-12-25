41824b70b963ac289daac350e250340d2a694dc8
William Grolleau <william.grolleau@cea.fr>
2024-12-25 08:36:09 +0100

    refactor(2024): day 23

--- 2024/day_23/main.py
+++ 2024/day_23/main.py
@@ -5,10 +5,7 @@ import networkx as nx
 input_file = "input"
 
 data = [x.strip().split("-") for x in open(input_file).readlines()]
-
-graph = nx.Graph()
-for x, y in data:
-    graph.add_edge(x, y)
+graph = nx.Graph(data)
 
 print(
     len([clique for clique in nx.enumerate_all_cliques(graph) if len(clique) == 3 and any(c[0] == "t" for c in clique)])
