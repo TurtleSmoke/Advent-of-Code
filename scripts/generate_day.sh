3dd8f74a9db281955f0acdd47f55574cd24dc6a1
TurtleSmoke <46165291+TuWG@users.noreply.github.com>
2025-12-02 20:11:16 +0100

    feat(2025): new year!

--- scripts/generate_day.sh
+++ scripts/generate_day.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-AOC_YEAR="${AOC_YEAR:-2024}"
+AOC_YEAR="${AOC_YEAR:-2025}"
 AOC_DAY="$1"
 AOC_SESSION=$(cat session_cookie)
 dir="$AOC_YEAR/day_$AOC_DAY"
@@ -11,5 +11,9 @@ AOC_DAY=${AOC_DAY#0}
 mkdir "$dir"
 touch "$dir/test_input"
 curl https://adventofcode.com/"$AOC_YEAR"/day/"$AOC_DAY"/input --cookie "session=$AOC_SESSION" -o "$dir/input"
-echo "#!/usr/bin/env python" >> "$dir/main.py"
+cat > "$dir/main.py" << 'EOF'
+#!/usr/bin/env python
+
+input_file = "test_input"
+EOF
 chmod u+x "$dir/main.py"
