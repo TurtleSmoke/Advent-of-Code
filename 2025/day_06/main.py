464d49b607fd90d80aa18483325ad19434ccb34f
TurtleSmoke <46165291+TuWG@users.noreply.github.com>
2025-12-06 21:09:38 +0100

    feat(2025): day 06

---
+++ 2025/day_06/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import re
+from itertools import zip_longest
+
+
+input_file = "test_input"
+
+operations = list(zip(*[s.split() for s in open(input_file).read().splitlines()]))
+operations2 = [
+    [x[0][:-1].strip(), *x[1:], x[0][-1]]
+    # Ugly, but the list below is "just" the list of columns...
+    for x in [
+        s.split("|")
+        for s in re.split(
+            r"\|\s+\|",
+            "|".join("".join(c) for c in zip_longest(*[line.rstrip("\n") for line in open(input_file)], fillvalue=" ")),
+        )
+    ]
+]
+
+solve = lambda data: sum(eval(op.join(nums)) for *nums, op in data)
+
+print(solve(operations))
+print(solve(operations2))
